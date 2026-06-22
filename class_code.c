f11356476f2090c814ce7bb04e09615c270cd2bd
rasel-space01 <raselhossain0063@gmail.com>
2026-06-22 13:41:08 +0600

    first commit

---
+++ class_code.c
@@ -0,0 +1,42 @@
+//?Just start
+
+/**
+ #include <stdio.h>   
+int main()          
+{
+    printf("Hello world!");
+    return 0;       
+}*/
+
+
+/* 
+*/
+
+//?first example(Format Specifiers) 
+
+/* 
+#include <stdio.h>
+int main(){
+    int age =22;
+    float height = 5.8;
+    char grade = 'A';
+    printf("age = %d\n",age);
+    printf("height = %f\n",height);
+    printf("grade = %c\n",grade);
+    return 0;
+
+}
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int x=25,y=20;
+    int z=x+y;
+    printf("The sum is = %d",z);
+    return 0;
+}
+
+
+
