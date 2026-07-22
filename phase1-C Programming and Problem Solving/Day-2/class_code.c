54c72fa97aa12551c393058557db6251b9b6e2df
rasel-space01 <raselhossain0063@gmail.com>
2026-07-22 20:55:13 +0600

    long long data type

--- phase1-C Programming and Problem Solving/Day-2/class_code.c
+++ phase1-C Programming and Problem Solving/Day-2/class_code.c
@@ -1,8 +1,21 @@
+//! int data type
+/*
 #include <stdio.h>
 int main(){
     int x= 2147483647 ;
     printf(" number is=%d\n",x);
     x=x+3;
     printf("update value is = %d\n",x);
+    return 0;
+}
+*/
+
+//! long long  data type
+#include <stdio.h>
+int main(){
+    long long x;
+    printf("Enter your net worth:");
+    scanf("%lld",&x);
+    printf("Your net worth is = %lld\n",x);
     return 0;
 }
