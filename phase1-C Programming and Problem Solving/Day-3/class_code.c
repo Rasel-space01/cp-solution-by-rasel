44423c051bd6050cd38527ea72e41076d62a555c
rasel-space01 <raselhossain0063@gmail.com>
2026-07-27 00:24:10 +0600

    A. Watermelon

---
+++ phase1-C Programming and Problem Solving/Day-3/class_code.c
@@ -0,0 +1,15 @@
+//!Class-5 (condition problem solving ,A. Watermelon from codeforces)
+ #include <stdio.h>
+ int main()
+ {
+    int w;
+    scanf("%d",&w);
+    if (w>2 && w%2==0)
+    {
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    return 0;
+ }
