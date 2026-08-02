0815c8740a6d3bc7e367d7414f6d4f7155255740
rasel-space01 <raselhossain0063@gmail.com>
2026-08-03 03:46:28 +0600

    loop with some problem solving

--- phase1-C Programming and Problem Solving/Class-7/Class-code.c
+++ phase1-C Programming and Problem Solving/Class-7/Class-code.c
@@ -1,6 +1,7 @@
 //! ✅✌Class-7: Practice Problems on Loop✅
-
-#include<stdio.h>
+ //? pyramid print
+ /*
+ #include<stdio.h>
 int main()
 {
     int n=21;
@@ -16,3 +17,176 @@ int main()
   }
   return 0;
 }
+ */
+
+//? Diamond print
+/*
+#include<stdio.h>
+int main()
+{
+    int n=21;
+  for( int i=1; i<=n; i+=2){
+    int space_count = (n-i)/2;
+    for( int j=1; j<=space_count; j++){
+        printf(" ");
+    }
+    for( int j=1;j<=i;j++){
+        printf("*");
+    }
+    printf("\n");
+  }
+
+   for( int i=n-2; i>=1; i-=2){
+    int space_count = (n-i)/2;
+    for( int j=1; j<=space_count; j++){
+        printf(" ");
+    }
+    for( int j=1;j<=i;j++){
+        printf("*");
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
+
+*/
+
+//! Prob-1: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_C
+
+/*
+#include<stdio.h>
+int main(){
+    while (1)
+    {
+        int x, y;
+
+    scanf("%d %d", &x, &y);
+    if( x==0 && y==0){
+        break;
+    }
+
+    if(x < y){
+        printf("%d %d\n", x, y);
+    }
+    else {
+        printf("%d %d\n", y, x);
+    }
+
+    }
+    
+    return 0;
+}
+*/
+
+ 
+//! Prob-2: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_B
+/*
+#include <stdio.h>
+
+int main(){
+    int case_no = 1;
+
+    while(1){
+        int x;
+        scanf("%d", &x);
+
+        if(x==0){
+            break;
+        }
+
+        printf("Case %d: %d\n", case_no, x);
+        case_no++;
+    }
+
+    return 0;
+}
+*/
+
+//! Prob-3: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_4_D
+
+/*
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    //int sum = 0;
+    long long sum=0;
+    int mn;
+    //int mx = -10000000;
+    int mx;
+
+    for(int i = 1; i <= n; i++){
+        int x;
+        scanf("%d", &x);
+
+        sum += x;
+
+        if(i==1){
+            mn = x;
+            mx =x;
+        }
+
+        if(x < mn){
+            mn = x;
+        }
+
+        if(x>mx){
+            mx =x;
+        }
+    }
+
+    printf("%d %d %ld\n", mn,mx,sum);
+    return 0;
+}
+*/
+//! Prob-4: https://lightoj.com/problem/greetings-from-
+/*
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+
+    for(int test_case = 1; test_case <= t; test_case++){
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        printf("Case %d: %d\n", test_case, a+b);
+    }
+
+    return 0;
+}
+*/
+
+ //?Same problem but shortcut:
+
+ /*
+ #include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
