34372201f5650f3f089dd22c201fbae704c4d6e3
rasel-space01 <raselhossain0063@gmail.com>
2026-08-02 18:30:16 +0600

    Add new thing in even and odd

--- phase1-C Programming and Problem Solving/Class-6/class_code.c
+++ phase1-C Programming and Problem Solving/Class-6/class_code.c
@@ -42,6 +42,7 @@ int main(){
 
 //! just start in for loop
 
+/*
 #include <stdio.h>
 int main()
 {
@@ -50,5 +51,43 @@ int main()
     }
     return 0;
 }
+*/
+
+//? 1 to 100 print
+/*
+ #include <stdio.h>
+int main()
+{
+    for ( int count=1; count<=100; count++){
+        printf("%d\n",count);
+    }
+    return 0;
+}
+ */
 
 
+//? 1 to 100 even(jur sonkha) number print 
+ 
+/*
+#include <stdio.h>
+int main()
+{
+    for ( int num=2; num<=100; num=num+2){  //num+=2 
+        printf("%d\n",num);
+    }
+    printf("loop Finished");
+    return 0;
+}
+*/
+
+
+//? 1 to 100 odd(bijur sonkha) number print
+#include <stdio.h>
+int main()
+{
+    for ( int num=1; num<=100; num=num+2){  //num+=2 
+        printf("%d\n",num);
+    }
+    printf("loop Finished");
+    return 0;
+}
