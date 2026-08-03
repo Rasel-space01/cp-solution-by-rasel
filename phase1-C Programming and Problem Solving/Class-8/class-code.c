ff85af633d183d8ae1ba94cb7ede09fd4bfdfe2c
rasel-space01 <raselhossain0063@gmail.com>
2026-08-03 17:15:00 +0600

    Array concept

---
+++ phase1-C Programming and Problem Solving/Class-8/class-code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int students[5]={60,56,63,65,87};
+
+    // students [0]=60;
+    // students [1]=56;
+    // students [2]=63;
+    // students [3]=65;
+    // students [4]=87;
+
+     printf("%d\n", students[3]);
+} 
