cec8bb142fadc98c6ec7e93b97d26a0dc29d5484
rasel-space01 <raselhossain0063@gmail.com>
2026-08-05 23:43:06 +0600

    array problem solving prob-2

--- phase1-C Programming and Problem Solving/Class-9/class-code.c
+++ phase1-C Programming and Problem Solving/Class-9/class-code.c
@@ -1,6 +1,6 @@
 //!✅ Practice Problems on Array & String Practice
 //!Problem-1: https://atcoder.jp/contests/abc290/tasks/abc290_a
-
+/*
 #include <stdio.h>
 
 int main()
@@ -25,7 +25,44 @@ int main()
 
 }
 
+*/
+
 //!✅ https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/I
   
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
+int main()
+{
+    char s1[1001];
+    scanf("%s", s1);
+
+    char s2[1001];
+    //s1 er reverse s2
+
+    int len = strlen(s1);
+    for(int i=0; i<len; ++i)
+    {
+        int j = len-1-i;
+        s2[i] = s1[j];
+    }
+    s2[len] = '\0';
+
+    bool equal = 1;
+    for(int i=0; i<len; ++i)
+    {
+        if(s1[i]!=s2[i]) equal = 0;
+    }
+
+    if(equal) //PALINDROME
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
+}
 
