1cbfe799a4dedf4447452fa65d78f87a24370b62
rasel-space01 <raselhossain0063@gmail.com>
2026-08-11 14:29:13 +0600

    all data type

--- C_by_books/Variables & Data Types/Books_code.c
+++ C_by_books/Variables & Data Types/Books_code.c
@@ -20,7 +20,46 @@ int main() {
 */
 
 //! Formate specifier(%d,%lf,%c,%f)
+//? int(%d)
+/*
+#include<stdio.h>
+int main()
+{
+    int age =25;
+    printf(" my age is: %d",age);
+    return 0;
+}
+*/
 
+//? float(%f)
 
+/*
+#include<stdio.h>
+int main() {
+float price = 99.50;
+printf("Price is %f", price);
+return 0;
+}
+*/
+
+//? Double(%lf)
+/*
+#include <stdio.h>
+int main() {
+double pi = 3.14159265358979;
+printf("PI = %.10lf", pi);
+return 0;
+}
+*/
+
+//? Char (%c)
+/*
+#include <stdio.h>
+int main() {
+char grade = 'A';
+printf("Your grade is %c", grade);
+return 0;
+}
+*/
 
 
