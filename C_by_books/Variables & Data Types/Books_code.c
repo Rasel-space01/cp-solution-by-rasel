b84c526c91c94a7f76289e5ab20092159d2d232a
rasel-space01 <raselhossain0063@gmail.com>
2026-08-11 14:18:06 +0600

    add some thing

---
+++ C_by_books/Variables & Data Types/Books_code.c
@@ -0,0 +1,26 @@
+//!✅✌First problem solve.
+
+/*
+#include<stdio.h>
+int main(){
+    printf("Hello world!");
+    return 0;
+}
+*/
+
+//! escape sequencelike (\n-new line, \t-tab space)
+/*
+#include <stdio.h>
+int main() {
+  printf("Hello\n");
+  printf("World");
+   return 0;
+}
+
+*/
+
+//! Formate specifier(%d,%lf,%c,%f)
+
+
+
+
