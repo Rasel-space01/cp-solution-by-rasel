7b1eb331742353c0c2b5ed4beb293e43c4164f84
rasel-space01 <raselhossain0063@gmail.com>
2026-08-17 21:57:27 +0600

    Start with example condition

--- C_by_books/Conditions/Books_code.c
+++ C_by_books/Conditions/Books_code.c
@@ -29,8 +29,8 @@ int main() {
     }
 */
 //?Example 1: Even or Odd (% operator)
-
-#include <stdio.h>
+/*
+ #include <stdio.h>
 int main() {
     int num = 7;
     if (num % 2 == 0) {
@@ -40,5 +40,33 @@ int main() {
     }
     return 0;
 }
+*/
+//? Grade system .
+
+#include <stdio.h>
+
+int main() {
+    int marks = 75;
+
+    if (marks >= 80) {
+        printf("Grade: A+\n");
+    } else if (marks >= 70) {
+        printf("Grade: A\n");
+    } else if (marks >= 60) {
+        printf("Grade: A-\n");
+    } else if (marks >= 50) {
+        printf("Grade: B\n");
+    } else if (marks >= 40) {
+        printf("Grade: C\n");
+    } else if (marks >= 33) {
+        printf("Grade: D\n");
+    } else {
+        printf("Grade: F\n");
+    }
+
+    return 0;
+}
+
+
 
 
